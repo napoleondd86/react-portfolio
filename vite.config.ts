8373b8d4b50bcd7fbd696cd82a84f8bbae359cd7
KWensmann <kurtwensmann@gmail.com>
2024-01-10 18:35:57 -0600

    "merge"
    "merge"

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import viteTsconfigPaths from 'vite-tsconfig-paths'
+
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), viteTsconfigPaths()],
+  plugins: [react()],
 })
